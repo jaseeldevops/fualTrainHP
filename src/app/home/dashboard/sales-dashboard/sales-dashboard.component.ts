d8753315b8698a6be02de3d352feabdaf378e910
MuhammedJaseel <muhammedjaseelm@Muhammeds-MacBook-Air-2.local>
2023-08-01 16:00:52 +0530

    Ordinery Push

--- src/app/home/dashboard/sales-dashboard/sales-dashboard.component.ts
+++ src/app/home/dashboard/sales-dashboard/sales-dashboard.component.ts
@@ -3,8 +3,69 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-sales-dashboard',
   templateUrl: './sales-dashboard.component.html',
-  styleUrls: ['./sales-dashboard.component.css']
+  styleUrls: ['./sales-dashboard.component.css'],
 })
 export class SalesDashboardComponent {
-
+  // <-START-> Site Status
+  // <END-> Site Status
+  //
+  // <-START-> Tank & Ullage Status
+  tankAndUllage = {
+    selected: 0,
+    onChangeSelected: (v: any) => {
+      this.tankAndUllage.selected = v;
+    },
+  };
+  // <END-> Tank & Ullage Status
+  //
+  // <-START-> DU & Nozzle Status
+  duAndNozzzle = {
+    selected: 0,
+    onChangeSelected: (v: any) => {
+      this.duAndNozzzle.selected = v;
+    },
+    onChangeDropDown: (e: any) => {
+      console.log(e.target.value);
+    },
+  };
+  // <END-> DU & Nozzle Status
+  //
+  // <-START-> Todays Sales & Inventory
+  // <END-> Todays Sales & Inventory
+  //
+  // <-START-> Nozzle Utilisation
+  // <END-> Nozzle Utilisation
+  //
+  // <-START-> Inventory Forecate
+  // <END-> Inventory Forecate
+  //
+  // <-START-> Top 10 Sales
+  // <END-> Top 10 Sales
+  //
+  // <-START-> Critical Stock
+  // <END-> Critical Stock
+  //
+  // <-START-> Nano Status
+  // <END-> Nano Status
+  //
+  // <-START-> Stock Reconsilation
+  // <END-> Stock Reconsilation
+  //
+  // <-START-> Price Change & Exceptions
+  // <END-> Price Change & Exceptions
+  //
+  // <-START-> Nil Sales
+  // <END-> Nil Sales
+  //
+  // <-START-> ROMMS Complaint Status
+  // <END-> ROMMS Complaint Status
+  //
+  // <-START-> Sales & Inventory Trend
+  // <END-> Sales & Inventory Trend
+  //
+  // <-START-> TAR (Trade Account Receivable)
+  // <END-> TAR (Trade Account Receivable)
+  //
+  // <-START-> TT Receipts SAP vs Actual and TT in top T ransit
+  // <END-> TT Receipts SAP vs Actual and TT in top T ransit
 }
